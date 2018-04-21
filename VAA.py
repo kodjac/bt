530759a1c02ddfc2fad2557900f0cf717df3af03
Florian Rieger <rieger@itm.uni-karlsruhe.de>
2018-04-22 00:31:23 +0200

    started VAA with end-of-month trading

--- VAA.py
+++ VAA.py
@@ -1,12 +1,21 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 import backtrader as bt
 import datetime
+import pandas as pd # for last business day of the month
 import sys
+
+def dbg(func):
+    [print(a) for a in dir(func)]
+
+def get_date(fl):
+    # internally stored as float of ordinal timestamp
+    return datetime.date.fromordinal(int(fl))
 
 class FixedCommisionScheme(bt.CommInfoBase):
     ''' This is a simple fixed commission scheme '''
     params = (
-        ('commission', 5),
+        ('commission', 6.5),
+        ('commission', 0.0),
         ('stocklike', True),
         ('commtype', bt.CommInfoBase.COMM_FIXED),
         )
@@ -15,34 +24,62 @@ class FixedCommisionScheme(bt.CommInfoBase):
         return self.p.commission
 
 
+class Momentum13612W(bt.Indicator):
+    lines = ('momentum',)
+    # plotinfo(dict(plothlines=[0.0]))
+
+    def __init__(self):
+        month = 21
+        self.addminperiod(12*month)
+        # fixme get actual end of month closing prices
+        print('fixme: actual end prices')
+        print(self.last_businees_days)
+        p0 = self.data
+        p1 = self.data(-month*1)
+        p3 = self.data(-month*3)
+        p6 = self.data(-month*6)
+        p12 = self.data(-month*12)
+        self.lines.momentum = 12*(p0/p1 - 1) + 4*(p0/p3 - 1) + 2*(p0/p6 - 1) + (p0/p12 - 1)
+        super().__init__()
+
+
 class VAA(bt.Strategy):
     params = (
-              ('sma_period', 20),
-              ('exitbars', 5),
+              ('risk', ['SPY', 'EFA', 'EEM', 'AGG']),
+              ('cash', ['LQD', 'IEF', 'SHY']),
+              ('lazytrade', False),
              )
 
 
-    def log(self, txt, dt=None, doprint=False):
-        current_dt = dt or self.datas[0].datetime.date(0)
+    def log(self, txt, doprint=True):
+        current_dt = self.datas[0].datetime.date(0)
         if doprint:
             print('{} {}'.format(current_dt.isoformat(), txt))
 
 
     def __init__(self):
+        print(self.datas[0])
+        # dbg(self.datas[0])
         self.dclose = self.datas[0].close  # reference to feed-line
         self.order = None
-        self.buy_date = None
-        self.price = []
-        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.sma_period)
+        self.hold = False
+        self.M13612W = Momentum13612W()
+        from_date = get_date(self.datas[0].fromdate)
+        to_date = get_date(self.datas[0].todate)
+        self.last_businees_days = [d.date() for d in pd.date_range(from_date, to_date, freq='BM')]
+        # print('last business days:')
+        # [print(d) for d in self.last_businees_days]
 
 
     def notify_order(self, order):
         # is called for every order status change: Submitted, Accepted, Completed
         # self.order = None  # no more order, why here?
+        # self.log('order {}'.format(order.getstatusname()))
         if order.status == order.Completed:
             order_type = 'BUY' if order.isbuy() else 'SELL'
             self.log('{} order {}'.format(order_type, order.getstatusname()))
             self.order = None if order.issell() else self.order
+
 
     def notify_trade(self, trade):
         # its 0 for buying
@@ -51,56 +88,58 @@ class VAA(bt.Strategy):
 
 
     def next(self):
-        self.price.append(self.dclose[0])
-        # dclose, 0: current price >0: future prices = current prices, -1 yesterdays price
-        # len(dclose) gives the length of past days, printing shows the upcoming days ?!!
-
-        if (len(self.dclose) > 2 and not self.order
-                and self.dclose[0] < self.dclose[-1] 
-                and self.dclose[-1] < self.dclose[-2]):
-            self.log('ordered at {}'.format(self.dclose[0]))
-
-            self.buy_date = len(self)
-            self.order = self.buy()
-
-        elif self.order and len(self) >= self.buy_date + self.params.exitbars:
-            self.log('sold at {}'.format(self.dclose[0]))
-            self.buy_date = None
-            self.order = self.sell()
-
-        else:
-            
+        now = get_date(self.data_datetime[0])
+        if now in self.last_businees_days:
             self.log('close {:.2f}, ({:.2f})'.format(self.dclose[0], cerebro.broker.getvalue()))
+            mom = self.M13612W.momentum[0]
+            print(mom)
+            if mom > 0.0 and not self.hold:
+                print('>BUY')
+                self.hold = True # fixme dependent ...
+                self.buy()  #fixme how much
+
+            elif mom < 0.0 and self.hold:
+                print('>SELL')
+                self.hold = False # fixme dependent ...
+                self.sell()
+
+            else:
+                print('do nothing ...')
+
+
+
+    def start(self):
+        print('_'*80)
+        self.log('VAA-G{} start value {:.2f} EUR'.format(
+            len(self.params.risk), self.broker.getvalue()))
 
 
     def stop(self):
-        self.log('VAA-{} end value {:.2f} EUR'.format(self.params.exitbars, self.broker.getvalue()),
-                 doprint=True)
-                 
+        self.log('VAA-G{} end value {:.2f} EUR'.format(
+            len(self.params.risk), self.broker.getvalue()))
 
 
 if __name__ == '__main__':
 
-    alphavantage_key = 'HR91R8PS4P19GES7'
+    alphavantage_key = 'HR91R8PS4P19GES7'  # flo.rieger@gmx.net
     # print('_'*80)
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(5500)
     cerebro.broker.addcommissioninfo(FixedCommisionScheme())
-    cerebro.addstrategy(VAA, exitbars=7, sma_period=10)
-    # cerebro.optstrategy(VAA, exitbars=range(5,30,5))
+    cerebro.addstrategy(VAA)
+    # cerebro.optstrategy(VAA)
 
-    data = bt.feeds.YahooFinanceData(
-            dataname='^GSPC',
-            fromdate=datetime.datetime(2000, 1, 1),
-            todate=datetime.datetime(2000, 5, 25),
-            )
+    for name in ('SPY'):# 'EFA', 'EEM', 'AGG', 'LQD', 'IEF', 'SHY'):
+        data = bt.feeds.YahooFinanceData(
+                dataname=name,
+                fromdate=datetime.datetime(2016, 1, 1),
+                todate=datetime.datetime(2017, 12, 31),
+                )
 
-    cerebro.adddata(data)
-
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        cerebro.adddata(data)
 
     cerebro.run()
     # cerebro.plot()
 
 
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
