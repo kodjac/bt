f772e564687520b30c573bb7fa1baf6e64a6f807
Florian Rieger (ED/ESY4) <florian.rieger@de.bosch.com>
2018-04-27 08:01:07 +0200

    working indicator

--- custom_vaa.py
+++ custom_vaa.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+import sys
 # https://pandas.pydata.org/pandas-docs/stable/10min.html
 
 logging.basicConfig(format='%(levelname)s %(funcName)s | %(message)s', level=logging.DEBUG)
@@ -19,27 +21,42 @@ class DateSync():
 
 # _________________________________________________________________________________________________
 class Asset():
-    def __init__(self, name:str, data:pd.DataFrae, date:DataSync):
+    def __init__(self, name:str, data:pd.DataFrame, date:DateSync):
         self.name = name
         self.data = data  # panda dataframe as specified
         self.date = date  # a DateSync object
 
     @property
     def close(self):
-        return self.data.loc[self.date, ['close']]  # todays close price
+        return self.data.loc[self.today, ['close']][0]  # todays close price
 
     @property
     def open(self):
-        return self.data.loc[self.date, ['close']]  # todays close price
+        return self.data.loc[self.today, ['open']][0]  # todays close price
 
     @property
     def today(self):
         return self.date.date
 
     @property
-    def indicator13612W(self):
-        # todo algorithm on self.data, based on self.date
-        return 0
+    def last_bussiness_days(self):
+        data_start_date = self.data.index[0] 
+        data_end_date = self.data.index[-1]
+        return [d.date() for d in pd.date_range(data_start_date, data_end_date, freq='BM')]
+
+    @property
+    def indicator_13612W(self):
+        # todo get closest last_bussiness_day
+        # assume only called on a last_bussiness_day
+        start_idx = self.last_bussiness_days.index(self.today)
+        end_of_months = list(reversed(self.last_bussiness_days[start_idx-12:start_idx+1]))
+        # log.debug(f'end_of_months [{len(end_of_months)}]= {end_of_months} ')
+
+        emc = [self.data.loc[d].close for d in end_of_months]
+        p0, p1, p3, p6, p12 = emc[0], emc[1], emc[3], emc[6], emc[12]  # map for nice formula
+
+        # assign the momentum value to the linebuffer
+        return round(12*(p0/p1 - 1) + 4*(p0/p3 - 1) + 2*(p0/p6 - 1) + (p0/p12 - 1), 2)
 
 
 # _________________________________________________________________________________________________
@@ -77,16 +94,17 @@ class Position():
 
 # _________________________________________________________________________________________________
 class Strategy():
-    def __init__(self, cash, assets:list, date:DataSync):
+    def __init__(self, cash, assets:list, date:DateSync):
         self.assets = {a.name: a for a in assets}  # dict of Asset objects
         self.date = date  # DataSync object
         self.cash = cash
-        [a.date = self.date for a in assets]  # sync all assets date
+        for a in assets:  # sync all assets date
+            a.date = self.date
         self._positions = []
 
     @property
     def value(self):
-        return self.cash + [active position values]
+        return self.cash + sum(p.value for p in self.positions)
 
     @property
     def positions(self):
@@ -117,21 +135,47 @@ class Strategy():
 
 # _________________________________________________________________________________________________
 class VAA_Strategy(Strategy):
-    def __init__(self, assets:list, date:DataSync):
-        super().__init__(assets, date)
+    def __init__(self, assets:list, date:DateSync):
+        super().__init__(self, assets, date)
         self.risk_assets = ['SPY', 'AGG']
         self.cash_assets = ['SHY']
 
     def execute(self, date):
+        pass
 
 
 # _________________________________________________________________________________________________
+def read_yahoo_csv(file_path):
+    return pd.read_csv(file_path, index_col=0, usecols=[0, 1, 2, 3, 4, 6], parse_dates=True)
+
+# _________________________________________________________________________________________________
+# _________________________________________________________________________________________________
 if __name__ == '__main__':
+
+    start_date = pd.to_datetime('2007-02-28')
+    log.debug('start_date = {} '.format(start_date))
+    date = DateSync(start_date)
+
     # todo define assets from files
+    assets = ['SPY', 'AGG', 'SHY']  # list of names
+    assets = [Asset(a, read_yahoo_csv(f'data/{a}.dat'), date) for a in assets]  # read the data
+
     # todo strategy object
+    strategy = VAA_Strategy(assets, date)
+    # start_date = VAA.assets.items()[0].data[idx0] + 13 months  # pre-run time for indicator
+
     # todo main loop
-    start_date = VAA.assets.items()[0].data[idx0] + 13 months
-    for date in panda.timeframe
+
+    for day in assets[0].last_bussiness_days[13:]:
+        log.debug(f'last trading day: {day} ____________________________________________________')
+        date.date = day  # update all dates
+        log.debug(f'day = {day}')
+
+        agg = strategy.assets['AGG']
+
+        strategy.assets['AGG'].indicator_13612W
+        log.debug(strategy.assets['AGG'].indicator_13612W)
+
 
     # buy: create position and try to buy it
     # sell: 
